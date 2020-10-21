87465f3826038e27b909bbea45f5f6513c2c4da3
tosini <marco.tosini@bitrock.com>
2020-10-21 14:24:57 +0200

    exercise

--- enum_/src/bin/channel.rs
+++ enum_/src/bin/channel.rs
@@ -4,11 +4,9 @@ use std::time::Duration;
 
 fn main() {
 
-
     let (transmitter, receiver) = mpsc::channel();
 
-
-    let trasmitter_clone = mpsc::Sender::clone(&transmitter);
+    let transmitter_clone = mpsc::Sender::clone(&transmitter);
 
     thread::spawn(move || {
         let message = String::from("messaggio");
@@ -17,7 +15,7 @@ fn main() {
 
     thread::spawn(move || {
         let message = String::from("messaggio2");
-        trasmitter_clone.send(message).unwrap();
+        transmitter_clone.send(message).unwrap();
     });
 
 
--- enum_/src/bin/mutex.rs
+++ enum_/src/bin/mutex.rs
@@ -1,5 +1,5 @@
-
-use std::sync::Mutex;
+use std::sync::{Mutex,Arc};
+use std::thread;
 
 fn main() {
 
@@ -11,5 +11,28 @@ fn main() {
     }
 
     println!("m = {:?}", m);
+
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut threads = vec![];
+
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let th =thread::spawn(move || {
+            let mut val = counter.lock().unwrap();
+            *val += 1;
+        });
+
+        threads.push(th);
+    }
+
+
+    for th in threads {
+        th.join().unwrap();
+    }
+
+    println!("{:?}", *counter.lock().unwrap());
+
 
 }
